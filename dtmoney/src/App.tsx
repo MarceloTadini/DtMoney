28dd213c7728cca77aeffd942e6c7c393c3a9a1d
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-11-18 15:08:57 -0300

    Modal com react e parâmetros alterando o 'pai'

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,32 @@
 import { DashBoard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-
+import Modal from 'react-modal';
+import { useState } from "react";
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setisNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setisNewTransactionModalOpen(false);
+    }
+
   return (
     <>
-     <Header/>
-     <DashBoard/>
-     <GlobalStyle/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <DashBoard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+      <GlobalStyle />
     </>
   );
 }
--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logoImg from '../../assets/logo.svg' //O nome da imagem é importado como o nome seguido de img dentro do seu diretório 
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="submit">
+                <button type="submit" onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
             </Content>
