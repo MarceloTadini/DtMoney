a1cbe8fbe2b9cb35ed85cf33e50e76561636ad39
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-11-22 15:30:22 -0300

    Cálculo do sumário

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -7,14 +7,32 @@ import { TransactionsContext } from "../../TransactionsContexts";
 
 export function Summary(){
 
-    const {transactions} = useContext(TransactionsContext);
+     const {transactions} = useContext(TransactionsContext);
 
-    const totalDeposit = transactions.reduce((acc, transaction) => {
-        if (transaction.type === 'deposit'){
-            return acc + transaction.amount;
+    // const totalDeposit = transactions.reduce((acc, transaction) => {
+    //     if (transaction.type === 'deposit'){
+    //         return acc + transaction.amount;
+    //     }
+    //     return acc;
+    // }, 0);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else{
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
         }
+
         return acc;
-    }, 0)
+    },
+    {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+
+    });
 
     return(
        <Container>
@@ -24,21 +42,38 @@ export function Summary(){
                    <p>Entradas</p>
                    <img src={incomeImg} alt="Entradas" />
                </header>
-               <strong>{totalDeposit}</strong>
+               <strong>
+                   {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+
+                    }).format(summary.deposits)}
+                </strong>
            </div>
            <div>
                <header>
                    <p>Saídas</p>
                    <img src={outcomeImg} alt="Saídas" />
                </header>
-               <strong> - R$500,00</strong>
+               <strong>
+                   - {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+
+                    }).format(summary.withdraws)} 
+                </strong>
            </div>
            <div className="highlight-background">
                <header>
                    <p>Total</p>
                    <img src={totalImg} alt="Total" />
                </header>
-               <strong>R$500,00</strong>
+               <strong>
+                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+                }).format(summary.total)}
+               </strong>
            </div>
        </Container> 
     );
